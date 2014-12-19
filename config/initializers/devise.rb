55b95844b2c49b13813757f6d2fe8aa27255b362
Kenneth Lin <klin@luckyruby.com>
2014-12-19 14:28:41 -0500

    Enable basic auth in devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -56,10 +56,10 @@ Devise.setup do |config|
 
   # Tell if authentication through HTTP Auth is enabled. False by default.
   # It can be set to an array that will enable http authentication only for the
-  # given strategies, for example, `config.http_authenticatable = [:database]` will
+  # given strategies, for example, `config.thenticatable = [:database]` will
   # enable it only for database authentication. The supported strategies are:
   # :database      = Support basic authentication with authentication key + password
-  # config.http_authenticatable = false
+  config.http_authenticatable = true
 
   # If 401 status code should be returned for AJAX requests. True by default.
   # config.http_authenticatable_on_xhr = true
