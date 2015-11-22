220831d6d488425a582a29e53ba16a5c85b5ba81
Kenneth Lin <klin@luckyruby.com>
2015-11-22 02:32:37 -0500

    fix migration bug

--- db/migrate/20141212153711_add_confidence_interval_to_players.rb
+++ db/migrate/20141212153711_add_confidence_interval_to_players.rb
@@ -1,7 +1,7 @@
 class AddConfidenceIntervalToPlayers < ActiveRecord::Migration
   def up
     add_column :players, :confidence_interval, :decimal, precision: 4, scale: 2
-    Player.update_aggregates
+    Player.update_statistics
   end
 
   def down
