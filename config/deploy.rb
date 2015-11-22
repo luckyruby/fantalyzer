5cd46b50565690b39e5988834b524327c8b832ea
Kenneth Lin <klin@luckyruby.com>
2015-11-22 02:20:01 -0500

    Add config.yml symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, "ssh://git@github.com/luckyruby/fantalyzer.git"
 set :branch, "master"
 set :deploy_to, "/var/www/fantalyzer"
 set :scm, :git
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/config.yml}
 set :linked_dirs, %w{log tmp}
 
 namespace :deploy do
